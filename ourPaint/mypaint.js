73d4dd0f0b3682affae046f036f428b8edbb4b73
Weyllan <valentin.bequart@isen-lille.fr>
2017-01-11 13:27:32 +0100

    fix and clear paint

--- ourPaint/mypaint.js
+++ ourPaint/mypaint.js
@@ -19,7 +19,6 @@ function createCanvas() {
 
 
     var canva = $(mycanva);
-    console.log(canva);
 
     //Création du context
     context = canva[0].getContext('2d');
@@ -31,7 +30,6 @@ function createCanvas() {
     mycanva.addEventListener("mousedown", function(e) {
         painting = true;
         var canva = $(mycanva);
-        console.log(canva);
 
         //Création du context
         context = canva[0].getContext('2d');
@@ -42,7 +40,6 @@ function createCanvas() {
 
     window.addEventListener("mouseup", function(e) {
         var canva = $(mycanva);
-        console.log(canva);
 
         //Création du context
         context = canva[0].getContext('2d');
@@ -53,7 +50,6 @@ function createCanvas() {
     mycanva.addEventListener("mousemove", function(e) {
         // Si je suis en train de dessiner (click souris enfoncé) :
         var canva = $(mycanva);
-        console.log(canva);
 
         //Création du context
         context = canva[0].getContext('2d');
@@ -72,7 +68,6 @@ function createCanvas() {
 // Fonction qui dessine une ligne :
 function drawLine() {
     // Si c'est le début, j'initialise
-    console.log(context);
     if (!started) {
         // Je place mon curseur pour la première fois :
         context.beginPath();
@@ -86,10 +81,6 @@ function drawLine() {
         context.lineWidth = width_brush;
         context.stroke();
     }
-}
-// Clear du Canvas :
-function clear_canvas() {
-    context.clearRect(0, 0, canvas.width(), canvas.height());
 }
 
 // Pour chaque carré de couleur :
@@ -108,30 +99,4 @@ $("#couleurs a").each(function() {
 
         return false;
     });
-});
-
-// Largeur du pinceau :
-$("#largeurs_pinceau input").change(function() {
-    if (!isNaN($(this).val())) {
-        width_brush = $(this).val();
-        $("#output").html($(this).val() + " pixels");
-    }
-});
-
-// Bouton Reset :
-$("#reset").click(function() {
-    // Clear canvas :
-    clear_canvas();
-
-    // Valeurs par défaut :
-    $("#largeur_pinceau").attr("value", 5);
-    width_brush = 5;
-    $("#output").html("5 pixels");
-
-});
-
-// Bouton Save :
-$("#save").click(function() {
-    var canvas_tmp = document.getElementById("canvas"); // Ca merde en pernant le selecteur jQuery
-    window.location = canvas_tmp.toDataURL("image/png");
 });
