057bf1d1ed26cfbeb98b3b1810872d71ee2afd7a
Weyllan <valentin.bequart@isen-lille.fr>
2017-01-12 11:43:00 +0100

    only one postit in auto

--- js/dragTest.js
+++ js/dragTest.js
@@ -1,7 +1,8 @@
 var initIntoPostitWidth;
 var initIntoPostitHeight;
 var inAutoDisplay = 0;
-
+var autoDisplayOccupate = false;
+var initPosX, initPosY;
 
 function allowDrop(event) {
     event.preventDefault();
@@ -10,27 +11,36 @@ function allowDrop(event) {
 function drag(event) {
     var style = window.getComputedStyle(event.target, null);
     var str = (parseInt(style.getPropertyValue("left")) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top")) - event.clientY) + ',' + event.target.id;
+    initPosX = event.clientX;
+    initPosY = event.clientY;
     event.dataTransfer.setData("text", str);
-
 }
 
 function drop(event) {
-    inAutoDisplay = 1;
-    event.preventDefault();
-    var offset = event.dataTransfer.getData("text").split(',');
-    var dm = document.getElementById(offset[2]);
-    console.log("OK", initIntoPostitWidth);
-    console.log(dm.offsetWidth);
-    if (dm.classList[1] != "intoAuto") {
-        initIntoPostitWidth = dm.offsetWidth;
-        initIntoPostitHeight = dm.offsetHeight;
-        event.target.appendChild(document.getElementById(offset[2]));
-        dm.style.height = "100%";
-        dm.style.width = "100%";
-        dm.classList[1] = "intoAuto";
-        dm.style.left = 0;
-        dm.style.top = 0;
+    if (!autoDisplayOccupate) {
+        inAutoDisplay = 1;
+        event.preventDefault();
+        var offset = event.dataTransfer.getData("text").split(',');
+        var dm = document.getElementById(offset[2]);
+        if (dm.classList[1] != "intoAuto") {
+            initIntoPostitWidth = dm.offsetWidth;
+            initIntoPostitHeight = dm.offsetHeight;
+            event.target.appendChild(document.getElementById(offset[2]));
+            dm.style.height = "100%";
+            dm.style.width = "100%";
+            dm.classList[1] = "intoAuto";
+            dm.style.left = 0;
+            dm.style.top = 0;
+        }
+        autoDisplayOccupate = true;
+    } else {
+        event.preventDefault();
+        var offset = event.dataTransfer.getData("text").split(',');
+        var dm = document.getElementById(offset[2]);
+        dm.style.left = initPosX + 'px';
+        dm.style.top = initPosY + 'px';
     }
+
 }
 
 function drop_outside(event) {
@@ -38,12 +48,11 @@ function drop_outside(event) {
         var offset = event.dataTransfer.getData("text").split(',');
         var dm = document.getElementById(offset[2]);
         if (dm.classList[1] === "intoAuto") {
-            console.log("IN", initIntoPostitWidth);
-            // DEMANDER A ADRIEN POUR LE -5
             dm.style.height = initIntoPostitHeight - 6 + "px";
             dm.style.width = initIntoPostitWidth - 6 + "px";
             dm.style.left = (event.clientX) + 'px';
             dm.style.top = (event.clientY) + 'px';
+            autoDisplayOccupate = false;
         } else {
             dm.style.left = (event.clientX + parseInt(offset[0], 10)) + 'px';
             dm.style.top = (event.clientY + parseInt(offset[1], 10)) + 'px';
@@ -54,7 +63,6 @@ function drop_outside(event) {
             event.target.appendChild(document.getElementById(offset[2]));
         }
         event.preventDefault();
-        console.log(dm.offsetWidth);
         return false;
     } else {
         inAutoDisplay = 0;
